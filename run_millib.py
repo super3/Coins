f031819b6d833ce7fe2cc243107470a979b75a1e
super3 <me@super3.org>
2013-10-25 19:28:00 -0400

    Add Schema

--- run_millib.py
+++ run_millib.py
@@ -8,7 +8,16 @@ app.config.from_object(__name__)
 
 # Flask Config -----------------------------------------------------------------
 DATABASE = 'millib.db'
+DEBUG = True
 
 # Database Functions -----------------------------------------------------------
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+if __name__ == '__main__':
+    app.run()
