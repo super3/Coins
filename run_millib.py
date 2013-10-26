5e5ae2d345517709a19e7e8feb3f2ed8fdd2991f
super3 <me@super3.org>
2013-10-25 20:20:14 -0400

    Working Website?

--- run_millib.py
+++ run_millib.py
@@ -25,11 +25,10 @@ def teardown_request(exception):
 	if hasattr(g, 'db'):
 		g.db.close()
 
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+# Routes -----------------------------------------------------------------------
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
